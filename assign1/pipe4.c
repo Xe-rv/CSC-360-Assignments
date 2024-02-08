ba0c8360a46381a30a25a8dbe6ebe30862543ffe
xerv <xerv@uvic.ca>
2024-02-08 01:17:44 +0000

    nearly completed version of pipe4

--- assign1/pipe4.c
+++ assign1/pipe4.c
@@ -28,87 +28,96 @@
 #define MAX_CMD 4
 
 int counter = 0; // keeps track of amount of inputs
-char* cmd_args[MAX_CMD][MAX_ARG]; // array of tokenized inputs
+char input[MAX_CMD][MAX_LEN]; // stores all of the users inputs
 int i;
 
-void arg_tok(char* cmdline) {
+// tokenizes inputs and executes the command
+void exe_tok(char* cmdline) {
+    char* cmd_args[MAX_ARG]; // array of tokenized inputs
     char* token;
     int num_tokens = 0;
 
     token = strtok(cmdline, " ");
     while(token != NULL && num_tokens < MAX_ARG - 1) {
-        cmd_args[counter][num_tokens++] = token;
+        cmd_args[num_tokens] = token;
+        num_tokens++;
         token = strtok(NULL, " ");
     }
 
-    cmd_args[counter][num_tokens] = NULL;
-    
+    cmd_args[num_tokens] = NULL;
+
+    execvp(cmd_args[0], cmd_args);
 }
 
 // stores up to 4 user inputs into an array
 void get_inputs() {
-    char input[MAX_CMD][MAX_LEN]; // array of user inputs
-    
     // stores user inputs till it hits max or until user enters to exit
     for(i = 0; i < MAX_CMD; i++) {
         read(0, input[i], MAX_LEN);
 
         // leaves loop when user just enters without an input
-        if(input[i] == NULL || strcmp(input[i], "\n") == 0) {
+        if(input[i][0] == '\n') {
             break;
         }
 
         input[i][strlen(input[i])-1] = '\0';
-        arg_tok(input[i]);
         counter++;
     }
 }
 
 int main() {
-    int fd[counter][2]; // number of pipes required depending on number of inputs
+    int pipes;
     pid_t pid;
+    pid_t child_pids[MAX_CMD];
     int status;
 
     // gets all user inputs
     get_inputs();
 
-    for(i = 0; i < counter - 1; i++) {
-        pipe(fd[i]);
-    }
+    // number of pipes required depending on number of inputs
+    pipes = counter - 1;
+    int fd[2*pipes]; 
 
     // loops through each cmd and pipes output to the next cmd
     // does this till it gets to the final cmd where it outputs the final cmd
     for(i = 0; i < counter; i++) {
-        pid = fork();
-        if(pid == 0) {
-            // redirects stdout to pipe out
-            if (i > 0) {
-                dup2(fd[i][0], 0);
-                close(fd[i][0]);
-                close(fd[i][1]);
-            }
+        pipe(fd + i * 2);
 
-            // redirects stdin to pipe in
-            if(i < counter - 1) {
-                dup2(fd[i+1][1], 1);
-                close(fd[i+1][1]);
-                close(fd[i+1][0]);
+        // creates the child 
+        pid = fork();
+
+        if(pid == 0) { // child pipe
+            
+            // redirects stdout to next pipe but only if its not the final cmd
+            if(i < pipes) {
+                dup2(fd[i*2+1], 1);
             } 
             
+            // redirects stdin from previous pipe but only if its not the first cmd
+            if (i > 0) {
+                dup2(fd[(i-1)*2], 0);
+            }
+
+            // closes every child pipe
+            for(int j = 0; j < 2 * pipes; j++) {
+                close(fd[j]);
+            }
+            
             // executes the cmd arguments 
-            execvp(cmd_args[i][0], cmd_args[i]);
+            exe_tok(input[i]);
+        }  else {// Store the process ID of the child process
+            child_pids[i] = pid;
         }
         
     }
     
     // closes every parent pipe
-    for(i = 0; i < counter - 1; i++) {
-        close(fd[i][0]);
-        close(fd[i][1]);
+    for(i = 0; i < 2 * pipes; i++) {
+        close(fd[i]);
     }
 
     // waits for child process
     for(i = 0; i < counter; i++) {
-        waitpid(pid, &status, 0);
+        waitpid(child_pids[i], &status, 0);
     }
 }
